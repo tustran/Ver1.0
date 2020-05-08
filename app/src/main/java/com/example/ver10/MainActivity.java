a94f1cb9541a85034fbe6f81214a43f39126bc49
trungtinh961 <39235800+trungtinh961@users.noreply.github.com>
2020-05-08 11:31:58 +0700

    initial

--- app/src/main/java/com/example/ver10/MainActivity.java
+++ app/src/main/java/com/example/ver10/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
