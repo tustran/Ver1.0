b5c3484a3acf8bf16a6e8c137f5b010a32465542
trungtinh961 <39235800+trungtinh961@users.noreply.github.com>
2020-06-07 17:45:24 +0700

    initial

--- app/src/main/java/com/example/ver10/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/ver10/ui/gallery/GalleryFragment.java
@@ -22,8 +22,8 @@ public class GalleryFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        View root = inflater.inflate(R.layout.fragment_area, container, false);
+        final TextView textView = root.findViewById(R.id.text_area);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
--- app/src/main/java/com/example/ver10/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/ver10/ui/slideshow/SlideshowFragment.java
@@ -22,8 +22,8 @@ public class SlideshowFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
+        View root = inflater.inflate(R.layout.fragment_setting, container, false);
+        final TextView textView = root.findViewById(R.id.text_setting);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
