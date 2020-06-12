0ab6e8bd6ff3e7f9d0bc5934178b17cdd2635b95
Tran Minh Tu <tu.tranminh@hcmut.edu.vn>
2020-06-12 17:35:34 +0700

    UIinit

---
+++ app/src/main/java/com/example/ver10/AreadetailFragment.java
@@ -0,0 +1,14 @@
+package com.example.ver10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AreadetailFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areadetail_fragment);
+    }
+}
--- app/src/main/java/com/example/ver10/ui/gallery/AreaFragment.java
+++ app/src/main/java/com/example/ver10/ui/gallery/AreaFragment.java
@@ -23,13 +23,13 @@ public class AreaFragment extends Fragment {
         areaViewModel =
                 ViewModelProviders.of(this).get(AreaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_area, container, false);
-        final TextView textView = root.findViewById(R.id.text_area);
-        areaViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_area);
+//        areaViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
