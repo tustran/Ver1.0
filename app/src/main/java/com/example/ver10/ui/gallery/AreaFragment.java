e6cbeaaebea19ff3ad63727b9fad2cc57d960dc2
Tran Minh Tu <tu.tranminh@hcmut.edu.vn>
2020-06-12 17:50:37 +0700

    initxl

--- app/src/main/java/com/example/ver10/ui/gallery/AreaFragment.java
+++ app/src/main/java/com/example/ver10/ui/gallery/AreaFragment.java
@@ -1,9 +1,11 @@
 package com.example.ver10.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.ver10.AreaDetailFragment;
 import com.example.ver10.R;
 
 public class AreaFragment extends Fragment {
@@ -22,12 +25,21 @@ public class AreaFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         areaViewModel =
                 ViewModelProviders.of(this).get(AreaViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_area, container, false);
-        final TextView textView = root.findViewById(R.id.text_area);
-        areaViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final View root = inflater.inflate(R.layout.fragment_area, container, false);
+//        final TextView textView = root.findViewById(R.id.text_area);
+//        areaViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        final Button testButton = (Button) root.findViewById(R.id.button1);
+        testButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Intent intenta = new Intent(this, );
+                startActivity(intenta);
             }
         });
         return root;
