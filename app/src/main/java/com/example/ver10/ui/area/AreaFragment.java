796b244b2b309006985ac2f887d77b2c45794a48
Tran Minh Tu <tu.tranminh@hcmut.edu.vn>
2020-06-14 10:12:43 +0700

    listsensorUInew2

--- app/src/main/java/com/example/ver10/ui/area/AreaFragment.java
+++ app/src/main/java/com/example/ver10/ui/area/AreaFragment.java
@@ -6,14 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ver10.R;
@@ -25,10 +26,13 @@ public class AreaFragment extends Fragment {
     private AreaViewModel areaViewModel;
     ArrayList<Product> listProduct;
     ArrayList<Sensor> listSensor;
+    ArrayList<Motor> listMotor;
     ProductListViewAdapter productListViewAdapter;
     SensorListViewAdapter sensorListViewAdapter;
+    MotorListViewAdapter motorListViewAdapter;
     ListView listViewProduct;
     ListView listViewSensor;
+    ListView listViewMotor;
 //    Model phần tử dữ liệu hiện
     class Product {
         String name;
@@ -54,18 +58,27 @@ public class AreaFragment extends Fragment {
         }
 
     }
-    class Headersensor {
-        String Temperature;
-        String Moisture;
-        String sensorID;
+    class Motor {
 
-        public Headersensor() {
-            this.Temperature = "Độ ẩm";
-            this.Moisture = "Nhiệt độ";
-            this.sensorID = "Cảm biến";
+        int motorID;
+        boolean status;
+
+        public Motor(int motorID, boolean status) {
+            this.motorID = motorID;
+            this.status = status;
+        }
+        public void setOn(){
+            this.status=true;
+        }
+        public void setOff(){
+            this.status=false;
+        }
+        public boolean getStatus(){
+            return this.status;
         }
 
     }
+
 
     class ProductListViewAdapter extends BaseAdapter {
 
@@ -217,6 +230,65 @@ public class AreaFragment extends Fragment {
         }
     }
 
+    class MotorListViewAdapter extends BaseAdapter {
+
+        //Dữ liệu liên kết bởi Adapter là một mảng các cảm biến
+        final ArrayList<Motor> listMotor;
+
+        MotorListViewAdapter(ArrayList<Motor> listMotor) {
+            this.listMotor = listMotor;
+        }
+
+        @Override
+        public int getCount() {
+            //Trả về tổng số phần tử, nó được gọi bởi ListView
+            return listMotor.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            //Trả về dữ liệu ở vị trí position của Adapter, tương ứng là phần tử
+            //có chỉ số position trong listProduct
+            return listMotor.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            //Trả về một ID của phần
+            return listMotor.get(position).motorID;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //convertView là View của phần tử ListView, nếu convertView != null nghĩa là
+            //View này được sử dụng lại, chỉ việc cập nhật nội dung mới
+            //Nếu null cần tạo mới
+
+            final View viewMotor;
+            if (convertView == null) {
+                viewMotor = View.inflate(parent.getContext(), R.layout.motor_view, null);
+            } else viewMotor = convertView;
+
+            //Bind sữ liệu phần tử vào View
+//            TextView textID  = viewMotor.findViewById(R.id.text_id);
+            final Motor motor = (Motor) getItem(position);
+            ((Switch) viewMotor.findViewById(R.id.switch_id)).setText(String.format("%s", motor.motorID+"           "));
+            ((Switch) viewMotor.findViewById(R.id.switch_id)).setChecked(motor.status);
+//            ((TextView) viewSensor.findViewById(R.id.sensortemp)).setText(String.format("%s oC", sensor.Temperature));
+//            ((TextView) viewSensor.findViewById(R.id.sensormois)).setText(String.format("%s percent", sensor.Moisture));
+            ((Switch) viewMotor.findViewById(R.id.switch_id)).setOnCheckedChangeListener(
+                    new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton,
+                                                     boolean b) {
+                            Toast.makeText(AreaFragment.super.getContext(),"Switch is " + (motor.status ? "On" : "Off"),Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+            return viewMotor;
+        }
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         areaViewModel =
@@ -269,6 +341,20 @@ public class AreaFragment extends Fragment {
         listSensor.add(new Sensor(5, "27", "30"));
         listSensor.add(new Sensor(6, "27", "20"));
         listSensor.add(new Sensor(7, "27", "20"));
+        listSensor.add(new Sensor(18, "27", "20"));
+        listSensor.add(new Sensor(29, "27", "20"));
+        listSensor.add(new Sensor(38, "27", "20"));
+        listSensor.add(new Sensor(42, "29", "20"));
+        listSensor.add(new Sensor(53, "27", "30"));
+        listSensor.add(new Sensor(63, "27", "20"));
+        listSensor.add(new Sensor(72, "27", "20"));
+        listSensor.add(new Sensor(11, "27", "20"));
+        listSensor.add(new Sensor(12, "27", "20"));
+        listSensor.add(new Sensor(13, "27", "20"));
+        listSensor.add(new Sensor(14, "29", "20"));
+        listSensor.add(new Sensor(15, "27", "30"));
+        listSensor.add(new Sensor(16, "27", "20"));
+        listSensor.add(new Sensor(17, "27", "20"));
 
         sensorListViewAdapter = new SensorListViewAdapter(listSensor);
 
@@ -299,6 +385,27 @@ public class AreaFragment extends Fragment {
 //                }
 //            }
 //        });
+        //Khoi tao ListProduct
+        listMotor = new ArrayList<Motor>();
+        listMotor.add(new Motor(1, false));
+        listMotor.add(new Motor(2,true));
+
+
+        motorListViewAdapter = new MotorListViewAdapter(listMotor);
+
+        listViewMotor = root.findViewById(R.id.listmotor);
+        listViewMotor.setAdapter(motorListViewAdapter);
+
+
+        //Lắng nghe bắt sự kiện một phần tử danh sách được chọn
+        listViewMotor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Motor motor = (Motor) motorListViewAdapter.getItem(position);
+                //Làm gì đó khi chọn sản phẩm (ví dụ tạo một Activity hiện thị chi tiết, biên tập ..)
+                Toast.makeText(AreaFragment.super.getContext(), "khong co gi ca", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
         return root;
