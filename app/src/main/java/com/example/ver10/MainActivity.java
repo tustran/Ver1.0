c0ca7ee337b0eadc7ae3929a1887e3b0a34d176b
Tran Minh Tu <tu.tranminh@hcmut.edu.vn>
2020-06-16 17:40:26 +0700

    Revert "update mqtt"
    This reverts commit 3150b945fcb17cf18e4d3fd0a500b906a0d11912.

--- app/src/main/java/com/example/ver10/MainActivity.java
+++ app/src/main/java/com/example/ver10/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.ver10;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -13,72 +12,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
-import java.nio.charset.Charset;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    int counter = 0;
-
-    MQTTHelper mqttHelper;
-    public void startMQTT(){
-        mqttHelper = new MQTTHelper(getApplicationContext());
-        mqttHelper.setCallback(new MqttCallbackExtended() {
-            @Override
-            public void connectComplete(boolean b, String s) {
-
-            }
-
-            @Override
-            public void connectionLost(Throwable throwable) {
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-                Log.w("Debug",mqttMessage.toString());
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
-            }
-        });
-    }
-
-    public void sendDataToMQTT(final String ID, final String value1, final String value2){
-        final Timer aTimer = new Timer();
-        TimerTask aTask = new TimerTask() {
-            @Override
-            public void run() {
-                MqttMessage msg = new MqttMessage();
-                msg.setId(1234);
-                msg.setQos(0);
-                msg.setRetained(true);
-
-                //String data = ID + ":[" + value1 + "," + value2 + "]";
-                String data = "[{\"device_id\":\"Speaker\", \"values\":[\"" + value1 + "\",\"" + value2 + "\"]}]";
-                byte[] b = data.getBytes(Charset.forName("UTF-8"));
-                msg.setPayload(b);
-
-                try {
-                    mqttHelper.mqttAndroidClient.publish("Topic/Speaker", msg);
-                    Log.e("publish","published");
-
-                }catch (MqttException e){
-                }
-            }
-        };
-        aTimer.schedule(aTask, 10000, 10000);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+//        FloatingActionButton add = findViewById(R.id.add);
+//        add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -98,10 +41,14 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-        startMQTT();
-        sendDataToMQTT("Speaker","0","0");
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
