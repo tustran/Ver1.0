e88f748eebe583e0a56de5a232f9794cfcba446e
Tran Minh Tu <tu.tranminh@hcmut.edu.vn>
2020-06-16 17:52:51 +0700

    Revert "Revert "update mqtt""
    This reverts commit c0ca7ee3

--- app/src/main/java/com/example/ver10/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ver10/ui/home/HomeFragment.java
@@ -11,17 +11,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.example.ver10.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -30,6 +27,10 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+
         return root;
     }
 }
